6c0676145d5b2d4ec94f50fd0ccfd55521990d37
Nyx <alexanderbeyer112@gmail.com>
2024-12-03 19:26:39 +0100

    day1 of aoc24

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,63 +1,44 @@
+use std::iter::zip;
+
 use aoclib::{input, output};
 
 fn main() {
     let input = input("input");
-
-    let num_lines = input.lines().count();
-    let mut left = Vec::with_capacity(num_lines);
-    let mut right = Vec::with_capacity(num_lines);
+    let mut vec_left = Vec::new();
+    let mut vec_right = Vec::new();
+    // Data preprocessing code goes here
     for line in input.lines() {
-        let (l, r) = line.split_once("   ").unwrap();
-        let l: u32 = l.parse().unwrap();
-        let r: u32 = r.parse().unwrap();
-
-        left.push(l);
-        right.push(r);
+        let (left, right) = line.split_once("   ").unwrap();
+        vec_left.push(left.parse::<u32>().unwrap());
+        vec_right.push(right.parse::<u32>().unwrap());
+        vec_left.sort();
+        vec_right.sort();
     }
 
-    p1(&mut left, &mut right);
-    // vecs are now sorted
-    p2(left, right);
+    p1(&vec_left, &vec_right);
+    p2(&vec_left, &vec_right);
 }
 
 #[inline]
-fn p1(left: &mut [u32], right: &mut [u32]) {
-    // Sorting does not need to be stable - relative order does not matter.
-    left.sort_unstable();
-    right.sort_unstable();
-
-    let result: u32 = left
-        .iter()
-        .zip(right.iter())
-        .map(|(&l, &r)| l.abs_diff(r))
-        .sum();
-
-    output(result);
+fn p1(vec_left: &Vec<u32>, vec_right: &Vec<u32>) {
+    let iter = zip(vec_left, vec_right);
+    let result: u32 = iter.map(|(a, b)| a.abs_diff(*b)).sum();
+    output(result)
 }
 
 #[inline]
-fn p2(left: Vec<u32>, right: Vec<u32>) {
-    // as the list are sorted we can traverse the list linearly and not miss any number.
-    // we start all the way at the start.
-    let mut r_idx = 0;
-    let mut sum = 0;
-    let mut prev_l = 0;
-    let mut count = 0;
 
-    for l in left {
-        // update counter if l != prev_l
-        if l != prev_l {
-            count = 0;
-            while r_idx < right.len() && l >= right[r_idx] {
-                if l == right[r_idx] {
-                    count += 1;
-                }
-                r_idx += 1;
+fn p2(vec_left: &[u32], vec_right: &[u32]) {
+    let mut sum = 0u32;
+    for n in vec_left.iter() {
+        let mut count = 0u32;
+        for i in vec_right.iter() {
+            // print!("{i}");
+            if *n == *i {
+                count += 1;
             }
         }
-        sum += l * count;
-        prev_l = l;
+        sum += count * n;
     }
-
     output(sum);
 }
